5bb202dc926dcde7fcd6cb6b7bd62b83aa939d1f
Jean Matheus Vassalo Ferreira <jeanvassaloferreira@gmail.com>
2020-04-20 13:52:32 -0300

    Success and Failure pages

--- app.js
+++ app.js
@@ -37,14 +37,19 @@ app.post("/", function(req,res){
 
     request(options, function(error,response,body){
         if(error){
-            console.log(error);            
+            res.sendFile(__dirname+"/failure.html")            
+        }else if(response.statusCode===200){
+            res.sendFile(__dirname+"/success.html")
         }else{
-            console.log(response.statusCode);
-            
+            res.sendFile(__dirname+"/failure.html")  
         }
     })
 
     //console.log(firstName, lastName, email)
+})
+
+app.post("/failure", function(req,res){
+    res.redirect("/")
 })
 
 app.listen(3000, function(){
